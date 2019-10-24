22b0c42198a966b593b0022f783040090bd8b070
Ilya Sidorchik <ilya@sidorchik.ru>
2019-10-24 21:22:30 +0300

    Add routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './pages/App/App';
+import App from './pages/App/';
 import './index.scss';
 
 ReactDOM.render(<App />, document.querySelector('.Root'));
--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
+import Search from '../Search';
+import MyPage from '../MyPage';
+import FriendPage from '../FriendPage';
+import FriendsList from '../FriendsList';
 import './App.scss';
 
 const App: React.FC = () => {
-	return <div className="App">Hello, world!</div>;
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Switch>
+					<Route path="/" component={Search} exact />
+					<Route path="/my-page" component={MyPage} />
+					<Route path="/friend-page" component={FriendPage} />
+					<Route path="/friends-list" component={FriendsList} />
+					<Redirect to="/" />
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
