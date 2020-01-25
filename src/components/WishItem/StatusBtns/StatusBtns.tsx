0b95f387dd8c3d049b779c64f0c08769f635e106
Ilya Sidorchik <ilya@sidorchik.ru>
2020-01-25 18:09:51 +0300

    Update StatusBtns

--- src/components/WishItem/StatusBtns/StatusBtns.tsx
+++ src/components/WishItem/StatusBtns/StatusBtns.tsx
@@ -1,36 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import Button from '../../Button';
-import './StatusBtns.scss';
+import Button from "../../Button";
+import "./StatusBtns.scss";
 
 interface IStatusBtns {
-	isWanted?: boolean;
-	handleClick: () => void;
+    isWanted?: boolean;
+    handleClick: () => void;
 }
 
 const StatusBtns: React.FC<IStatusBtns> = ({
-	isWanted = false,
-	handleClick
+    isWanted = false,
+    handleClick
 }) => (
-	<div className="StatusBtns">
-		{!isWanted && (
-			<Button className="StatusBtns-Favourite" onClick={handleClick}>
-				Добавить в избранное
-			</Button>
-		)}
+    <div className="StatusBtns">
+        {!isWanted && (
+            <Button className="StatusBtns-Favourite" onClick={handleClick}>
+                Добавить в избранное
+            </Button>
+        )}
 
-		{isWanted && (
-			<Button
-				className="StatusBtns-Favourite Button_default"
-				onClick={handleClick}
-			>
-				Не подарю{' '}
-				<span role="img" aria-label="Эмодзи «Задумчивое лицо»">
-					😔
-				</span>
-			</Button>
-		)}
-	</div>
+        {isWanted && (
+            <Button
+                className="StatusBtns-Favourite Button_default"
+                onClick={handleClick}
+            >
+                Не хочу
+                <span role="img" aria-label="Эмодзи «Задумчивое лицо»">
+                    😔
+                </span>
+            </Button>
+        )}
+    </div>
 );
 
 export default StatusBtns;
