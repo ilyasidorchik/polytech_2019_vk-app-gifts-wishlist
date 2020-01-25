4ed77c7cc3810e3b54459ea767f229a28d25a0b0
Ilya Sidorchik <ilya@sidorchik.ru>
2020-01-25 21:50:11 +0300

    Add Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,40 +4,40 @@ import cx from 'classnames';
 import './Input.scss';
 
 interface InputProps extends HTMLAttributes<HTMLElement> {
-	className?: string;
-	name: string;
-	type?: string;
-	placeholder?: string;
-	value: string;
-	onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
-	autoFocus?: boolean;
+    className?: string;
+    name: string;
+    type?: string;
+    placeholder?: string;
+    value: string;
+    onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+    autoFocus?: boolean;
 }
 
 const Input: React.FC<InputProps> = ({
-	className,
-	name,
-	type,
-	placeholder,
-	value,
-	onChange,
-	autoFocus
+    className,
+    name,
+    type,
+    placeholder,
+    value,
+    onChange,
+    autoFocus
 }) => {
-	return (
-		<input
-			className={cx('Input', className)}
-			type={type}
-			name={name}
-			placeholder={placeholder}
-			aria-label={placeholder}
-			value={value}
-			onChange={onChange}
-			autoFocus={autoFocus}
-			autoComplete="off"
-			autoCorrect="off"
-			autoCapitalize="off"
-			spellCheck={false}
-		/>
-	);
+    return (
+        <input
+            className={cx('Input', className)}
+            type={type}
+            name={name}
+            placeholder={placeholder}
+            aria-label={placeholder}
+            value={value}
+            onChange={onChange}
+            autoFocus={autoFocus}
+            autoComplete="off"
+            autoCorrect="off"
+            autoCapitalize="off"
+            spellCheck={false}
+        />
+    );
 };
 
 export default Input;
